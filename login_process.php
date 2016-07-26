fa7f970dab65c3ef84b7fe2687828b2cdf170a28
PCGYEFASIS04 <PCGYEFASIS04@PCGYEFASIS04-PC>
2016-07-26 16:50:40 -0500

    Cambio de index

--- login_process.php
+++ login_process.php
@@ -8,9 +8,11 @@
 
 session_start();
 require_once './Conexion.php';
+$obj = new Conexion();
 
 if(isset($_POST['btn-login']))
 {
+    echo "PRUEBA DESDE LOGIN_PROCESS";
     $user = $_POST['username'];
     $password = $_POST['password'];
     
@@ -22,7 +24,7 @@ if(isset($_POST['btn-login']))
    
     if($row[0] == "1"){
     // Guardo en la sesión el user del usuario.
-        echo '1';
+        echo "1";
         $_SESSION['user'] = $user;
         // Redirecciono al usuario a la página principal del sitio.
         //header("HTTP/1.1 302 Moved Temporarily");
